08b679e9770cf0dbb9fda89c77d526cabd7278df
leungju6 <75376050+leungju6@users.noreply.github.com>
2020-12-02 10:29:23 -0500

    Update autobooker.py

--- autobooker.py
+++ autobooker.py
@@ -59,7 +59,9 @@ try:
     # check available_slots class 2nd index -> see if child elements exist
     available_slots = driver.find_elements_by_class_name("available-slots")[1].find_elements_by_class_name(
         "time-slot-box")
-
+    if len(available_slots) = 0:
+        print("No available time slots!")
+        
     for slot in available_slots:
         a_slot = str(slot.text).split(" ")[4] + str(slot.text).split(" ")[5].split('\n')[0]
         if str(os.getenv("TIME_SLOT")) == a_slot:
